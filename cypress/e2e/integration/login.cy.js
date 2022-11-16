3c078706a719159a20314bc430c7fe98aab17d28
Ivan Consalter <ivanconsalter1@gmail.com>
2022-11-16 18:31:11 -0300

    login

--- cypress/e2e/integration/login.cy.js
+++ cypress/e2e/integration/login.cy.js
@@ -1,8 +1,12 @@
 /// <reference types="cypress" />
 
-describe('Deve fazer login.', () => {
+describe('Curso Cypress', () => {
 
   it('deve fazer login', () => {
-    cy.visit('https://barrigareact.wcaquino.me/');
+    cy.visit(Cypress.env('baseUrl'));
+    cy.get('[data-test="email"]').type(Cypress.env('login'));
+    cy.get('[data-test="passwd"]').type(Cypress.env('password'));
+    cy.get('.btn').click();
+    cy.get('.toast-message').should('contain', 'Bem vindo');
   });
 });
