cead518358a6d5f5f6066296c8047d96f1c723e9
Ivan Consalter <ivanconsalter1@gmail.com>
2022-11-17 17:25:13 -0300

    test nova conta

--- cypress/e2e/integration/login.cy.js
+++ cypress/e2e/integration/login.cy.js
@@ -2,11 +2,19 @@
 
 describe('Curso Cypress', () => {
 
-  it('deve fazer login', () => {
+  before( () => {
     cy.visit(Cypress.env('baseUrl'));
     cy.get('[data-test="email"]').type(Cypress.env('login'));
     cy.get('[data-test="passwd"]').type(Cypress.env('password'));
     cy.get('.btn').click();
     cy.get('.toast-message').should('contain', 'Bem vindo');
+  });
+
+  it('deve inserir uma conta', () => {
+    cy.get('[data-test="menu-settings"]').click();
+    cy.get('[href="/contas"]').click();
+    cy.get('[data-test="nome"]').type('Teste Conta');
+    cy.get('.btn').click();
+    cy.get('.toast-message').should('contain', 'Conta inserida com sucesso');
   });
 });
