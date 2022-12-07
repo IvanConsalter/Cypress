daa7daea5b27ea3aed332298610cfe80537e9f85
Ivan Consalter <ivanconsalter1@gmail.com>
2022-12-07 16:27:32 -0300

    added test login rest

--- cypress/e2e/integration/backend.cy.js
+++ cypress/e2e/integration/backend.cy.js
@@ -3,11 +3,21 @@
 describe('Teste Api Rest', () => {
 
   before( () => {
-    cy.login(Cypress.env('login'), Cypress.env('password'));
-    cy.resetApp();
+    // cy.login(Cypress.env('login'), Cypress.env('password'));
+    // cy.resetApp();
   });
 
   it('deve inserir uma conta', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://barrigarest.wcaquino.me/signin',
+      body: {
+        email: Cypress.env('login'),
+        redirecionar: false,
+        senha: Cypress.env('password')
+      }
+    })
+    .its('body.token').should('not.be.empty')
   });
   
   it('deve editar uma conta', () => {
